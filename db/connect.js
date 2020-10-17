45eafac4bd7b0027243a34f8057f6cff746b625a
liu <1059623688@qq.com>
2020-10-17 08:53:04 +0800

    完整上传文件夹 建立目录

---
+++ db/connect.js
@@ -0,0 +1,17 @@
+/* 
+// 加载mongoose模块，用于在node中操作数据库
+const mongoose = require('mongoose')
+
+// 连接数据库
+mongoose.connect('mongodb://localhost/2005',{ useNewUrlParser: true, useUnifiedTopology: true })
+
+// 保存连接数据库对象
+let db = mongoose.connection
+
+// 监听连接对象的error事件，连接数据库失败时触发
+db.on('error', console.error.bind(console, 'connection error:'))
+
+// 监听连接对象的连接成功事件，连接数据库成功时触发
+db.once('open', function() {
+  console.log('----------db connect ok------------')
+}) */
---
+++ utils/mail.js
@@ -0,0 +1,43 @@
+
+const nodemailer = require("nodemailer")
+
+// 创建发送邮件对象
+let transporter = nodemailer.createTransport({
+  host: "smtp.qq.com",// 邮件发送者的邮箱主机(见下方查找)
+  port: 465,// 端口号
+  secure: true, // 端口号为465是为true,其他端口为fales
+  auth: {
+    user: '99835885@qq.com', // 邮件发送者的邮箱地址，如:99835885@qq.com
+    pass: 'xwgkatgigvvubjcb', // 邮件发送者的邮箱smtp授权码(见下方设置)
+  },
+})
+
+function send(email,title,message){
+  // 发送邮件的配置信息
+  let info = {
+    from: '"xx站管理员" <99835885@qq.com>', // 发送者的邮箱地址
+    to: email, // 接收邮件者列表
+    subject: title, // 邮件主题
+    text: message, // 纯文本邮件内容(二选一)
+    // html: "<b>Hello world?</b>", // html格式邮件内容(二选一)
+  }
+
+  // 发送邮件
+  return new Promise((resolve,reject)=>{
+    transporter.sendMail(info,(err)=>{
+      if (err) {
+        // 发送失败
+        reject()
+      } else {
+        // 发送成功
+        resolve()
+      }
+    })
+  })
+}
+
+module.exports = {send}
+
+// 封装一个发送邮件的模块
+// const mail = require('mail')
+// mail.send('接收邮件者地址','邮件主题','邮件内容')
